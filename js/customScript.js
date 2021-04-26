3820fba6acd649d145d888e873146701760648ef
Toka Aly <toaaali72@gmail.com>
2021-04-27 00:21:47 +0200

    first-commit

--- js/customScript.js
+++ js/customScript.js
@@ -252,3 +252,22 @@ jQuery(function() {
         }
     });
 });
+
+document.getElementById("defaultOpen").click();
+$(document).ready(function() {
+    $('#vertical_tab_nav > .vertical-links > .link > a').eq(0).addClass("selected");
+    $('#vertical_tab_nav > div > .content').eq(0).css('display', 'block');
+    $('#vertical_tab_nav > .vertical-links ').click(function(e) {
+        if ($(e.target).is("a")) {
+            /*Handle Tab Nav*/
+            $('#vertical_tab_nav > .vertical-links  > .link  > a').removeClass("selected");
+            $(e.target).addClass("selected");
+            /*Handles Tab Content*/
+            var clicked_index = $("a", this).index(e.target);
+            $('#vertical_tab_nav > div > .content').css('display', 'none');
+            $('#vertical_tab_nav > div > .content').eq(clicked_index).fadeIn();
+        }
+        $(this).blur();
+        return false;
+    });
+});
